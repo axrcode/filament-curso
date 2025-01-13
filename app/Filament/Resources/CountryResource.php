f4ea8d12e48c104dbfbbd99ffb34bb22689e8fe2
axrcode <axrcode@gmail.com>
2025-01-13 14:34:29 -0600

    add: iconos y grupo para menu de opciones

--- app/Filament/Resources/CountryResource.php
+++ app/Filament/Resources/CountryResource.php
@@ -17,7 +17,9 @@ class CountryResource extends Resource
 {
     protected static ?string $model = Country::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'System Management';
+    protected static ?int $navigationSort = 3;
+    protected static ?string $navigationIcon = 'heroicon-o-flag';
 
     public static function form(Form $form): Form
     {
